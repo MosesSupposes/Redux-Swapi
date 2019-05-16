5e7fb146a8ec060a3b9a189138e8fa973c1b6d47
Moses Samuel <mosamuel98@gmail.com>
2019-05-16 13:56:38 -0700

    apply thunk and logger middleware to redux store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+
+import "./index.css";
+
+import App from "./App";
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+
+
+
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
